7af4a755ff7f32e5f45890681f64eec8997adc1e
wsj <13823728026@163.com>
2016-02-23 09:19:54 +0800

    init

---
+++ SJPersonDetailPage/SJPersonDetailPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJPersonDetailPage
+//
+//  Created by app;e on 16/2/23.
+//  Copyright © 2016年 world. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
